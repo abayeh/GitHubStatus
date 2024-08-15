c947b140b04c47dea1cf3cfdfd0c9c1dd4c09010
abayeh <abayeh@gmail.com>
2024-08-15 17:40:00 -0500

    Initial Commit

---
+++ GitHubStatus/GitHubStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubStatusApp.swift
+//  GitHubStatus
+//
+//  Created by Alexander Bayeh on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
